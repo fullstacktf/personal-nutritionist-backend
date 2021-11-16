bd4e895360cfade67adf5ee14927f552a13f7d20
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-11-16 11:56:09 +0000

    Fixing copy files on dockerfile, upgrade go.mod to direct dependences and change run.sh script 👷‍♂️

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build . -t nutriguide-backend-image:test && \
-docker run --rm --name nutriguide-backend -p 8080:80 nutriguide-backend-image:test
+docker run --rm --name nutriguide-backend -p 8080:8080 nutriguide-backend-image:test
