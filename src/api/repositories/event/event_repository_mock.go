82653d439b240997dc603936ab3e9214dcfd00cf
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-12-07 10:43:07 +0000

    CreateEvent tests done ✅

--- src/api/repositories/event/event_repository_mock.go
+++ src/api/repositories/event/event_repository_mock.go
@@ -4,6 +4,7 @@ import (
 	"github.com/fullstacktf/personal-nutritionist-backend/api/models"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type EventRepositoryMock struct {
@@ -13,4 +14,9 @@ type EventRepositoryMock struct {
 func (m *EventRepositoryMock) GetEvents(c *gin.Context) ([]models.Event, error) {
 	args := m.Called(c)
 	return args.Get(0).([]models.Event), args.Error(1)
+}
+
+func (m *EventRepositoryMock) CreateEvent(c *gin.Context, event *models.Event) (primitive.ObjectID, error) {
+	args := m.Called(c, event)
+	return args.Get(0).(primitive.ObjectID), args.Error(1)
 }
