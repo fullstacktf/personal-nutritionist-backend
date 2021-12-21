67a66bccbd2806549e45a24e64bcb776174f8956
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-12-21 16:29:57 +0000

    Changing password

--- src/services/password.go
+++ src/services/password.go
@@ -3,6 +3,9 @@ package services
 import "golang.org/x/crypto/bcrypt"
 
 func GenerateHashPassword(password string) (string, error) {
+	if string(password[0]) == "$" && len(password) > 30 {
+		return password, nil
+	}
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
